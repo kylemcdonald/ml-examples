d09b9f38ef17aff0d0846a14c2b6cb374c82b033
Kyle McDonald <kyle@kylemcdonald.net>
2016-01-11 15:03:55 +0900

    automatically pull bvlc_googlenet

--- setup.sh
+++ setup.sh
@@ -8,3 +8,13 @@ git submodule init # registers submodules with git
 
 echo "Updating submodules."
 git submodule update # pulls content of submodule repos
+
+BVLC_GOOGLENET_FILE="models/bvlc_googlenet/bvlc_googlenet.caffemodel"
+BVLC_GOOGLENET_URL="http://dl.caffe.berkeleyvision.org/bvlc_googlenet.caffemodel"
+BVLC_GOOGLENET_DEPLOY_FILE="models/bvlc_googlenet/deploy.prototxt"
+BVLC_GOOGLENET_DEPLOY_URL="https://raw.githubusercontent.com/BVLC/caffe/master/models/bvlc_googlenet/deploy.prototxt"
+if [ ! -e $BVLC_GOOGLENET_FILE ]; then
+	echo "Downloading bvlc_googlenet..."
+	curl -o $BVLC_GOOGLENET_DEPLOY_FILE $BVLC_GOOGLENET_DEPLOY_URL
+	curl -o $BVLC_GOOGLENET_FILE $BVLC_GOOGLENET_URL
+fi
